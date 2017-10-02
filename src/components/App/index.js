954776b7c26bdbe56e167e55559a7106a69a663f
Tom Valorsa <tomvalorsa@gmail.com>
2017-10-02 20:43:58 +0100

    .

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react'
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
-import Leaflet from 'leaflet'
 import styles from './index.scss'
-import 'leaflet/dist/leaflet.css'
 
-Leaflet.Icon.Default.imagePath = 'leaflet-images/'
+const initialCenter = [51.505, -0.09]
+const initialZoom = 10
 
 export default class App extends Component {
   render() {
+    const markers = [
+      { id: 0, text: 'Emirates Stadium', position: [51.554888, -0.108438] },
+      { id: 1, text: 'Stamford Bridge', position: [51.481663, -0.190956] }
+    ]
+
     return (
       <div className={styles.fullscreen}>
-        <Map center={[51.505, -0.09]} zoom={13} className={styles.map}>
-          <TileLayer
-            url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          />
-          <Marker position={[51.505, -0.09]}>
-            <Popup>
-              <span>A pretty CSS3 popup.<br />Easily customizable.</span>
-            </Popup>
-          </Marker>
+        <Map center={initialCenter} zoom={initialZoom} className={styles.map}>
+          <TileLayer url='http://{s}.tile.osm.org/{z}/{x}/{y}.png' />
+          {markers.map(marker => {
+            return (
+              <Marker position={marker.position} key={marker.id}>
+                <Popup>
+                  <div>{marker.text}</div>
+                </Popup>
+              </Marker>
+            )
+          })}
         </Map>
       </div>
     )
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import { render } from 'react-dom'
+import Leaflet from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+Leaflet.Icon.Default.imagePath = 'leaflet-images/'
+
 import App from './components/App'
 import './index.scss'
 import './favicon.ico'
